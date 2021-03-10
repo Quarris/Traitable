e1f7ed03ff9041504d0b2cc35d6aa6c120bf8cf9
Quarris <tomasz.winnicki@live.co.uk>
2021-03-10 16:43:09 +0000

    Initial commit

---
+++ src/main/java/quarris/traitable/Traitable.java
@@ -0,0 +1,18 @@
+package quarris.traitable;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(Traitable.ID)
+public class Traitable {
+
+    public static final String ID = "traitable";
+
+    // Directly reference a log4j logger.
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public Traitable() {
+
+    }
+}
